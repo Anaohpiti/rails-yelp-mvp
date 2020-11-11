98824fca5405abc275342d6b1bdc5376309f0158
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-11 15:25:56 +0100

    yelp mvp is launched

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 5 fake Restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+      )
+  restaurant.save!
+end
+puts 'Finished!'
